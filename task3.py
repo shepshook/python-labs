30ca537c322ce5f5380977bead9e98454df1c390
shepshook <nikitashepshuk@gmail.com>
2020-02-13 11:15:52 +0300

    Add task #3

---
+++ task3.py
@@ -0,0 +1,21 @@
+import random
+
+def qsort(array):
+    less = []
+    equal = []
+    greater = []
+
+    if len(array) > 1:
+        pivot = array[0]
+        for num in array:
+            if num < pivot:
+                less.append(num)
+            elif num == pivot:
+                equal.append(num)
+            elif num > pivot:
+                greater.append(num)
+        return qsort(less) + equal + qsort(greater)
+    else:
+        return array
+
+print(qsort([random.randrange(1, 50, 1) for i in range(20)]))
