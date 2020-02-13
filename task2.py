d2ed29cb8b1725f6d888869236056c84a6011830
shepshook <nikitashepshuk@gmail.com>
2020-02-13 10:33:48 +0300

    Finish second task

--- task2.py
+++ task2.py
@@ -8,3 +8,7 @@ for word in words:
     else:
         dic[word] += 1
 
+sorted_words = [k for k, _ in sorted(dic.items(), key=lambda item: item[1], reverse=True)]
+
+for i in range(min(len(sorted_words), 10)):
+    print(sorted_words[i], end=" ")
